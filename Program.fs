0d3ecb96fd4141af987b674dfa582a708d9b8460
Eric Vincent <eric@efvincent.com>
2019-11-05 01:28:46 -0500

    trying to get fparsec working in fsx. TODO: ask in fparsec issues

--- Program.fs
+++ Program.fs
@@ -1,8 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open FParsec
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+  let p = pstring "test"
+  run p "input" |> printfn "%A"
+  printfn "\nbye!\n"
+  0 // return an integer exit code
