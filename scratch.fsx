f3c6d65a5a06363b4418380455a1ee9ea2e7e90c
Eric Vincent <eric@efvincent.com>
2019-11-05 01:38:42 -0500

    found the issue - order of references in fsi matters

--- scratch.fsx
+++ scratch.fsx
@@ -1,23 +1,14 @@
-#r @"bin/Debug/netcoreapp3.0/FParsec.dll"
 #r @"bin/Debug/netcoreapp3.0/FParsecCS.dll"
+#r @"bin/Debug/netcoreapp3.0/FParsec.dll"
 
 open FParsec
 open System
 
-let p = pstring "test"
-
-(*
-results in:
-
-/Users/.../code/scratch/fparsec-001/scratch.fsx(6,9): error FS0074: 
-The type referenced through 'FParsec.CharStream`1' is defined in an assembly 
-that is not referenced. You must add a reference to assembly 'FParsecCS'.
-
-*)
+let p : Parser<string,unit> = pstring "test"
 
 let test p str =
   match run p str with
   | Success(result,_,_) -> printfn "Success: %A" result
   | Failure(errMsg,_,_) -> printfn "Failure: %s" errMsg
 
-
+test p "input" 
