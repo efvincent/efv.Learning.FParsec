4708b5db5a826c8e4565710887b7d6a847b18852
Eric Vincent <eric@efvincent.com>
2019-11-06 02:12:52 -0500

    problem with last parser - stringLiteral*

--- scratch.fsx
+++ scratch.fsx
@@ -20,7 +20,7 @@ test pfloat "1.25E 3"
 // parsing a float between brackets
 
 let str s = pstring s
-let floatBetweenBrackets:Parser<'a,unit> = str "[" >>. pfloat .>> str "]"
+let floatBetweenBrackets:Parser<float,unit> = str "[" >>. pfloat .>> str "]"
 
 test floatBetweenBrackets "[1.0]"
 
@@ -44,7 +44,7 @@ test (many floatBetweenBrackets) "[2][3][34E]"
 
 test (many1 floatBetweenBrackets) ""
 
-let floatList:Parser<'a list,unit> = str "[" >>. sepBy pfloat (str ",") .>> str "]"
+let floatList:Parser<float list,unit> = str "[" >>. sepBy pfloat (str ",") .>> str "]"
 
 test floatList "[]"
 
@@ -63,6 +63,82 @@ let numberList = str_ws "[" >>. sepBy float_ws (str_ws ",") .>> str_ws "]"
 
 test numberList "[1,2,3]"
 
-test numberList 9
+test numberList "   [    3,   434    ,343 ,  345e9 ] "
 
-// NEXT: 4.7 Parsing String Data
+// 4.7 Parsing String Data
+
+test (many (str "a" <|> str "b")) "abbaaaaaba"
+
+test (skipStringCI "<float>" >>. pfloat) "<FloAT>1.0"
+
+let ident:Parser<string,unit> = 
+  let isIdentifierFirstChar c = isLetter c || c = '_'
+  let isIdentifierChar c = isLetter c || isDigit c || c = '_'
+  many1Satisfy2L isIdentifierFirstChar isIdentifierChar "identifier"
+  .>> ws
+
+///
+/// many1Satisfy2L - parses one or more characters (many1) where the first
+/// character satisfies the first predicate, and each of the rest satisfy
+/// the second predicate. The "L" at the end means it expects a label for
+/// the parser, useful in errors.
+/// 
+
+test ident "eric"
+test ident "__private21"
+test ident "33skadoo"
+  
+
+(*
+  given the grammar:
+  stringLiteral: '"' (normalChar|escapedChar)* '"'
+  normalChar:    any char except '\' and '"'
+  escapedChar:   '\\' ('\\'|'"'|'n'|'r'|'t')
+*)
+
+let stringLiteral:Parser<string,unit> =
+  let normalChar = satisfy (fun c -> c <> '\\' && c <> '"')
+  let unescape = function 
+  | 'n' -> '\n'
+  | 'r' -> '\r'
+  | 't' -> '\t'
+  | c -> c
+
+  let escapedChar = pstring "\\" >>. (anyOf "\\nrt\"" |>> unescape)
+  between (pstring "\"") (pstring "\"")
+    (manyChars (normalChar <|> escapedChar))
+
+let stringLiteral2:Parser<string,unit> =
+    let normalChar = satisfy (fun c -> c <> '\\' && c <> '"')
+    let unescape c = match c with
+                     | 'n' -> '\n'
+                     | 'r' -> '\r'
+                     | 't' -> '\t'
+                     | c   -> c
+    let escapedChar = pstring "\\" >>. (anyOf "\\nrt\"" |>> unescape)
+    between (pstring "\"") (pstring "\"")
+            (manyChars (normalChar <|> escapedChar))
+
+test stringLiteral2 "\"abc\""
+
+let stringLiteral3:Parser<string,unit> =
+  let normalCharSnippet = many1Satisfy (fun c -> c <> '\\' && c <> '"')
+  let escapedChar = 
+    pstring "\\" >>. 
+    (anyOf "\\nrt\"" 
+      |>> function
+          | 'n' -> "\n"
+          | 'r' -> "\r"
+          | 't' -> "\t"
+          | c   -> string c)
+  between (pstring "\"") (pstring "\"")
+          (manyStrings (normalCharSnippet <|> escapedChar))
+
+test stringLiteral3 "\"abc\""
+
+test stringLiteral3 "abc"
+
+(*
+  Note ... these stringLiteral parsers are not working
+  correctly, debug them
+*)
